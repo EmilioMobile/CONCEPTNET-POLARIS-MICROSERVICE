cb590c6975dd789e7b21fad01af135609d269356
Emilio Jose Coronado Lopez <emilio.mobile@gmail.com>
2018-12-11 13:04:24 +0900

    fix

--- src/controllers/smalltalk.controller.js
+++ src/controllers/smalltalk.controller.js
@@ -4,12 +4,7 @@ class PolarisXSmalltalk {
   chat (input) {
       return new Promise((resolve, reject) => {
         switch (input) {
-          case "what's the time": {
-            var d = new Date();
-            resolve('Now is ', d.getHours(), ':', d.getMinutes(), ':', d.getSeconds())
-          }
-
-          case "what is the time": {
+          case "what time is it": {
             var d = new Date();
             resolve('Now is ', d.getHours(), ':', d.getMinutes(), ':', d.getSeconds())
           }
@@ -21,8 +16,11 @@ class PolarisXSmalltalk {
           case "what is the weather today": {
             resolve('Today is cold, very very cold')
           }
+
+          default: {
+            resolve('Hi I am the small talk module, you wanted a answer for ' + input + '?')
+          }
         }
-          resolve('Hi I am the small talk module, you wanted a answer for ' + input + '?')
       })
   }
 }
